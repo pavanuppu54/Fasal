c6513e0b2cd9ee910fb8dd4d1dff9bf8777e5b0b
UPPU JYOTHI NAGA PAVAN <110449636+pavanuppu54@users.noreply.github.com>
2024-06-04 17:03:45 +0530

    Delete Fasal directory

--- Fasal/movies/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MovieList
-
-class MovieListForm(forms.ModelForm):
-    class Meta:
-        model = MovieList
-        fields = ['name', 'is_public']
--- Fasal/project/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS =True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='ramya9652414628@gmail.com'
-EMAIL_HOST_PASSWORD='apkm sfru abfu fyvf'
-EMAIL_PORT=587
