09411343bdd508edaa8ba161de97b232bdce6ab8
UPPU JYOTHI NAGA PAVAN <110449636+pavanuppu54@users.noreply.github.com>
2024-06-05 14:27:17 +0530

    Update info.py

--- project/info.py
+++ project/info.py
@@ -1,5 +1,8 @@
-EMAIL_USE_TLS =True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='ramya9652414628@gmail.com'
-EMAIL_HOST_PASSWORD='apkm sfru abfu fyvf'
-EMAIL_PORT=587
+# info.py
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ramya9652414628@gmail.com'
+EMAIL_HOST_PASSWORD = 'apkm sfru abfu fyvf'
+EMAIL_PORT = 587
+EMAIL_FEEDBACK_RECIPIENT = "ramya9652414628@gmail.com"
